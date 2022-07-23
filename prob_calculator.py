80becdbfffd1f7d88992286a6d3add08ac2cacc4
Tom Monks <monksy@gmail.com>
2022-07-23 04:52:26 -0400

    Cap max qty drawn and count balls

--- prob_calculator.py
+++ prob_calculator.py
@@ -10,7 +10,11 @@ class Hat:
             self.content += [key] * value
 
     def draw(self, qty):
-        drawn = [self.pop_random_item() for i in range(qty)]
+        if qty >= len(self.content):
+            drawn = self.content
+            self.content = []
+        else:
+            drawn = [self.pop_random_item() for i in range(qty)]
         return drawn
 
     def pop_random_item(self):
@@ -18,9 +22,17 @@ class Hat:
         return self.content.pop(index)
 
 
+def count_balls(balls):
+    counts = {}
+    for ball in balls:
+        counts[ball] = (counts.get(ball) or 0) + 1
+    return counts
+
+
 # def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 hat = Hat(blue=4, red=2, green=6)
 print(hat.content)
-draw = hat.draw(3)
+draw = hat.draw(33)
 print(draw)
-print(hat.content)
+print(count_balls(draw))
+# print(hat.content)
